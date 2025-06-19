f9c59f047e419bf5384651a367070b542db0220d
suhariky <Gtrnpy@yandex.ru>
2025-06-19 16:27:55 +0300

    added basic site

---
+++ site/static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('hello-btn').addEventListener('click', function() {
+        alert('Привет от JavaScript!');
+    });
+}); 
